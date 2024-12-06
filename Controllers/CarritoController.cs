ef958d24c10e7f3bfdaf088f66ede722321003ef
luis_cvqz <zs21013875@estudiantes.uv.mx>
2024-12-06 15:21:45 -0600

    Carrito controllers

--- Controllers/CarritoController.cs
+++ Controllers/CarritoController.cs
@@ -1,17 +1,98 @@
-using System.Security.Claims;
 using frontendnet.Models;
 using frontendnet.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace frontendnet;
 
 [Authorize(Roles = "Usuario")]
-public class CarritoController() : Controller
+public class CarritoController(CarritoClientService carrito, PedidosClientService pedidos) : Controller
 {
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        List<Producto>? lista = [];
+        try
+        {
+            //var carritoService = HttpContext.RequestServices.GetService<CarritoClientService>();
+            lista = await carrito.ObtenerCarrito();
+        }
+        catch (HttpRequestException ex)
+        {
+            if (ex.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                return RedirectToAction("Salir", "Auth");
+            }
+        }
+
+        return View(lista);
+    }
+
+    public async Task<IActionResult> SeguirComprando()
+    {
+        return RedirectToAction("Index", "Comprar");
+    }
+
+    public async Task<IActionResult> LimpiarCarrito()
+    {
+        try
+        {
+            //var carritoService = HttpContext.RequestServices.GetService<CarritoClientService>();
+            await carrito.LimpiarCarrito();
+        }
+        catch (HttpRequestException ex)
+        {
+            if (ex.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                return RedirectToAction("Salir", "Auth");
+            }
+        }
+        return RedirectToAction("Index", "Carrito");
+    }
+
+    public async Task<IActionResult> FinalizarCompra()
+    {
+        if (await carrito.ObtenerCarrito() == null)
+        {
+            return RedirectToAction("Index", "Carrito");
+        }
+
+        try
+        {
+            var userName = User.Identity?.Name;
+
+            if (string.IsNullOrEmpty(userName))
+            {
+                return RedirectToAction("Salir", "Auth");
+            }
+            
+            await pedidos.PostAsync(userName);
+        }
+        catch (HttpRequestException ex)
+        {
+            if (ex.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                return RedirectToAction("Salir", "Auth");
+            }
+        }
+
+        return RedirectToAction("Index", "Carrito");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> EliminarDelCarrito(int id)
+    {
+        try
+        {
+            await carrito.EliminarDelCarrito(id);
+        }
+        catch (HttpRequestException ex)
+        {
+            if (ex.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                return RedirectToAction("Salir", "Auth");
+            }
+        }
+
+        return RedirectToAction("Index", "Carrito");
     }
 }
--- Controllers/ComprarController.cs
+++ Controllers/ComprarController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace frontendnet;
 
 [Authorize(Roles = "Usuario")]
-public class ComprarController(ProductosClientService productos, IConfiguration configuration) : Controller
+public class ComprarController(CarritoClientService carrito, ProductosClientService productos, IConfiguration configuration) : Controller
 {
     public async Task<IActionResult> Index(string? s)
     {
@@ -26,5 +26,26 @@ public class ComprarController(ProductosClientService productos, IConfiguration 
         ViewBag.Url = configuration["UrlWebAPI"];
         ViewBag.search = s;
         return View(lista);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AgregarAlCarrito(int id)
+    {
+        try
+        {
+            var producto = await productos.GetAsync(id);
+            //var carritoService = HttpContext.RequestServices.GetService<CarritoClientService>();
+
+            await carrito.AgregarAlCarrito(producto);
+        } 
+        catch (HttpRequestException ex)
+        {
+            if (ex.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                return RedirectToAction("Salir", "Auth");
+            }
+        }
+
+        return RedirectToAction("Index", "Carrito");
     }
 }
